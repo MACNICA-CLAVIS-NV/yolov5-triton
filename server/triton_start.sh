db7011ad9f79e8dceeaba99c0c2b49250646b8a6
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2022-08-16 14:39:17 +0900

    Updated

--- server/triton_start.sh
+++ server/triton_start.sh
@@ -5,10 +5,11 @@ set -eu
 source ./triton_config.sh
 
 sudo docker run \
+    --gpus=1 \
     -it \
     --rm \
     --net=host \
     --runtime nvidia \
     -v ${MODEL_REPO_HOST_PATH}:${MODEL_REPO_CONTAINER_PATH} \
     "${IMAGE_NAME}:${PYTORCH_TAG}" \
-    ./bin/tritonserver --model-repository=${MODEL_REPO_CONTAINER_PATH} --backend-directory=./backends --backend-config=tensorflow,version=2 --model-control-mode=none
+    ./bin/tritonserver --model-repository=${MODEL_REPO_CONTAINER_PATH} --backend-directory=./backends --backend-config=tensorrt,coalesce-request-input=true --model-control-mode=none
