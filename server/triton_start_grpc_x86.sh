950e4fb22456b8565d5313085a8a66e2f5a1b967
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2022-08-23 15:47:40 +0900

    Updated

--- server/triton_start_grpc_x86.sh
+++ server/triton_start_grpc_x86.sh
@@ -12,11 +12,3 @@ sudo docker run \
     -v ${MODEL_REPO_HOST_PATH}:${MODEL_REPO_CONTAINER_PATH} \
     "nvcr.io/nvidia/tritonserver:${TRITON_TAG}" \
     ./bin/tritonserver --model-repository=${MODEL_REPO_CONTAINER_PATH} --backend-directory=./backends --backend-config=tensorrt,coalesce-request-input=true --model-control-mode=none --allow-grpc=true --grpc-port=8000 --allow-http=false
-
-# sudo docker run \
-#     --gpus=all \
-#     -it \
-#     --rm \
-#     --network=host \
-#     -v ${MODEL_REPO_HOST_PATH}:${MODEL_REPO_CONTAINER_PATH} \
-#     "nvcr.io/nvidia/tritonserver:${TRITON_TAG}"
